61dd3eab760d0dc0850a96718708f43258794cf2
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2023-01-02 21:39:36 +0300

    1)Layout yapısı 2)Resource

---
+++ 4_WpfForm_2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _4_WpfForm_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCustomConten_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Merhaba");
+        }
+
+        private void btnOtherWindow_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBoxItem selectedItem = (ComboBoxItem)cmbWindowType.SelectedItem;
+            string str = (string)selectedItem.Content;
+
+            Window window;
+            if (str == "Wrap Panel")
+                window = new WrapPanel();
+            else if (str == "Dock Panel")
+                window = new wDockPanel();
+            else if (str == "Grid")
+                window = new wGrid();
+            else if (str == "Grid 2")
+                window = new wGrid2();
+            else
+                window = new wUniformGrid();
+
+            window.ShowDialog();
+        }
+    }
+}
